e293d6dcf50af335ac53e3d4d5108bca0e787ef4
shuvikash <shuvikash.skp@gmail.com>
2018-06-14 16:25:21 +0530

    Added file for get and post in react

--- App.jsx
+++ App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 
+//App component
 class App extends React.Component {
 	constructor(props) {
         super(props);
         this.state = {
             title: "Users",
-            users: []
+            users: [],
+			name:''
         };
 		this.getUser = this.getUser.bind(this);
+		this.handleChange=this.handleChange.bind(this);
+		this.handleSubmit=this.handleSubmit.bind(this);
     }
+	//To Retrieve all the user from spring boot api through axios
 	getUser(){
 		
 		axios.get("http://localhost:8086/users")
@@ -18,25 +23,53 @@ class App extends React.Component {
 			this.setState({ users });
 		  })
 	}
-	updateUser(){
-		alert("User updated");
-	}
+	//set the updated(value on the form) name
+	handleChange(e) {
+		this.setState({ name: e.target.value });
+		//alert(e.target.value);
+		
+ 
+    }
+	//submit the data to spring boot api through axios
+    handleSubmit(e) {
+		//to prevent default action happen
+         e.preventDefault();
+	axios.post("http://localhost:8086/users", {userName:this.state.name})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+    }
+	
     render() {
+		
 	  return (
-         <div>
+        <div>
 			<button onClick={this.getUser}>GetAllUser</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			<button onClick={this.updateUser}>UpdateUser</button>
-			 <table>
+			 <br /><br />
+			 
+			<table>
 			 <tbody>
 			 <tr> 
-			 <th>Id</th> 
-			 <th>User Name</th>
+			 <th>Id &nbsp;&nbsp;&nbsp;</th>
+			 <th>UserName</th>
 			 </tr>
-				{ this.state.users.map((user,i) => <li key={'user_' + i}><tr><td>{user.id}</td></tr>  <tr><td> {user.userName}</td></tr></li>)}
+				{ this.state.users.map((user,i) => <tr key={'user_' + i}><td>{user.id} &nbsp;&nbsp;&nbsp;&nbsp;</td><td>{user.userName}</td></tr>)}
 			 </tbody>
-			 </table>
-         </div>
+			</table>
+			<br /><br />
+			 
+		  <form onSubmit={this.handleSubmit}>
+             <label>
+             UserName:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             <input type="text" name="name" onChange={this.handleChange} />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             </label>
+            <button type="submit">UpdateUser</button>
+          </form>
+		
+       </div>
       );
    }
 }
+// exporting the App component to render it from main.js
 export default App;
